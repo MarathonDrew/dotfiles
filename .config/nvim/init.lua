660f46e62f77a0f6a9167cf7af0fad699edb99d5
Drew hinderhofer <drew.hinderhofer@gmail.com>
2025-06-24 21:17:03 -0400

    removing tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -958,29 +958,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-    },
-    lazy = false,
-    keys = {
-      { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    },
-    opts = {
-      filesystem = {
-        window = {
-          mappings = {
-            ['\\'] = 'close_window',
-          },
-        },
-      },
-    },
-  },
-
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
