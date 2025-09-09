d4f5ae52bc8bb9c0c19a99a696ab41c532f7037b
Drew hinderhofer <drew.hinderhofer@gmail.com>
2025-09-09 19:56:35 -0400

    neovim line number brightening

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,10 +1,14 @@
 return {
-  "tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-  },
+	"tokyonight.nvim",
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+		on_highlights = function(hl)
+			hl.LineNrBelow = "dddddd"
+			hl.LineNrAbove = "dddddd"
+		end,
+	},
 }
